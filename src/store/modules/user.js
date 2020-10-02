52d2b2cf13bc2124b400271b91cbe83ba838d292
Anh nguyen <harrykai99@gmail.com>
2020-10-02 16:50:00 +0700

    update user store data

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,4 +1,5 @@
-import { login, logout, getInfo, postLogin } from '@/api/user'
+import { login, logout, postLogin, getInfoByToken } from '@/api/user'
+// , getInfo
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import router, { resetRouter } from '@/router'
 
@@ -62,9 +63,9 @@ const actions = {
   // get user info
   getInfo({ commit, state }) {
     return new Promise((resolve, reject) => {
-      getInfo(state.token).then(response => {
+      getInfoByToken().then(response => {
         const { data } = response
-
+        console.log(data)
         if (!data) {
           reject('Verification failed, please Login again.')
         }
